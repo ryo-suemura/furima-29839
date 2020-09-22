48a684197f77c2cfd1a6d35f623fd49f7db208d4
ryo-suemura <ryosuemura@gmail.com>
2020-09-22 12:21:40 +0900

    placeのActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :place
 
   validates :name, :text, :price, :category, :condition, :delivery_fee, :place, :days, :user_id, presence:true
-  validates :category_id, numericality: { other_than: 1}
+  validates :category_id, :condition_id, :delivery_fee_id, :place_id, numericality: { other_than: 1}
 end
