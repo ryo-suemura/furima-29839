eef54ae7b06b4a3db2495370c324cf37ecb5be4e
ryo-suemura <ryosuemura@gmail.com>
2020-09-22 12:15:50 +0900

    delivery_feeのActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_fee
 
   validates :name, :text, :price, :category, :condition, :delivery_fee, :place, :days, :user_id, presence:true
   validates :category_id, numericality: { other_than: 1}
