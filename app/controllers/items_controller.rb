6a6783527f8d43e0b3af4eeca4df5f81eb86100e
ryo-suemura <ryosuemura@gmail.com>
2020-09-23 11:28:33 +0900

    商品保存のコードを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  #def item_params
-    #params.require(:item).permit(:name, :text, :price, :image)
-  #end
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :image, :category_id, :condition_id, :delivery_fee_id, :place_id, :days_id, :user_id).merge(user_id: current_user.id)
+  end
+
 end
