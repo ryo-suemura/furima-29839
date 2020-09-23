c90bab98b70f17ee1bdde8b9e38f053c801b303f
ryo-suemura <ryosuemura@gmail.com>
2020-09-23 14:59:10 +0900

    JSによる数値計算の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :place
-  belongs_to_active_hash :days
+  belongs_to_active_hash :day
 
   validates :name, :text, :price, :category, :condition, :delivery_fee, :place, :days, :user_id, presence:true
   validates :category_id, :condition_id, :delivery_fee_id, :place_id, :days_id, numericality: { other_than: 1}
   validates :price, inclusion: { in: 300..9999999, message: "Out of setting range" }, format: { with: /\A[0-9]+\z/, message: "Half-Width number"}
 
-  
+
 end
