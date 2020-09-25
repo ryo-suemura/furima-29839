84f79bf66e209ccb7388cadfc260417ca09fba4d
ryo-suemura <ryosuemura@gmail.com>
2020-09-25 16:24:53 +0900

    商品詳細ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,9 +18,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:name, :text, :price, :image, :category_id, :condition_id, :delivery_fee_id, :place_id, :days_id, :user_id).merge(user_id: current_user.id)
+    params.require(:item).permit( :name, :text, :price, :image, :category_id, :condition_id, :delivery_fee_id, :place_id, :days_id, :user_id).merge(user_id: current_user.id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
+  has_many :items
   with_options presence: true do
     validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'contain @' }
     validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'include both letters and numbers' }
