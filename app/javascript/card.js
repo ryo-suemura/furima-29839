d197ba48582baacc133dbae621a18282042cf961
ryo-suemura <ryosuemura@gmail.com>
2020-10-05 14:41:39 +0900

    console.logの削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,7 +13,6 @@ const pay = () => {
       exp_month: formData.get("buyer_purchase[exp_month]"),
       exp_year: `20${formData.get("buyer_purchase[exp_year]")}`,
     };
-    console.log(card)
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
