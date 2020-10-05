922dd5f34865ebbcc9406662d406c3b39fba4777
ryo-suemura <ryosuemura@gmail.com>
2020-10-05 12:45:40 +0900

    単体テストコードの設定

--- spec/factories/buyer_purchases.rb
+++ spec/factories/buyer_purchases.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :buyer_purchase do
-    postal_code { '123-4567' }
-    prefecture { '2' }
-    city { '岐阜県' }
-    address { '岐阜1-1' }
-    building_name { 'アパート' }
-    telephone { '11122223333' }
+    token                    {"tok_abcdefghijk00000000000000000"}
+    postal_code              { '123-4567' }
+    place_id                 { '2' }
+    city                     { '岐阜県' }
+    address                  { '岐阜1-1' }
+    building_name            { 'アパート' }
+    telephone_number         { '11122223333' }
   end
 end
--- spec/models/buyer_purchase_spec.rb
+++ spec/models/buyer_purchase_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe BuyerPurchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @buyer_purchase = FactoryBot.build(:buyer_purchase)
+  end
+
+  describe '商品購入' do
+    context '商品購入がうまくいくとき' do
+      it '全ての項目が存在すれば購入できる' do
+        expect(@buyer_purchase).to be_valid
+      end
+    end  
+
+    context '商品購入がうまくいかないとき' do
+      it 'token情報がなければ購入できない' do
+        @buyer_purchase.token = ''
+        @buyer_purchase.valid?
+        expect(@buyer_purchase.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'postal_codeがなければ購入できない' do
+        @buyer_purchase.postal_code = ''
+        @buyer_purchase.valid?
+        expect(@buyer_purchase.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeにハイフンがなければ購入できない' do
+        @buyer_purchase.postal_code = '1234567'
+        @buyer_purchase.valid?
+        expect(@buyer_purchase.errors.full_messages).to include("Postal code input correctly")
+      end
+      it 'place_idがなければ購入できない' do
+        @buyer_purchase.place_id = '1'
+        @buyer_purchase.valid?
+        expect(@buyer_purchase.errors.full_messages).to include("Place must be other than 1")
+      end
+      it 'cityがなければ購入できない' do
+        @buyer_purchase.city = ''
+        @buyer_purchase.valid?
+        expect(@buyer_purchase.errors.full_messages).to include("City can't be blank")
+      end
+      it 'addressがなければ購入できない' do
+        @buyer_purchase.address = ''
+        @buyer_purchase.valid?
+        expect(@buyer_purchase.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'telephone_numberがなければ購入できない' do
+        @buyer_purchase.telephone_number = ''
+        @buyer_purchase.valid?
+        expect(@buyer_purchase.errors.full_messages).to include("Telephone number can't be blank")
+      end
+      it 'telephone_numberは11桁以内でなければ購入できない' do
+        @buyer_purchase.telephone_number = '111222233334'
+        @buyer_purchase.valid?
+        expect(@buyer_purchase.errors.full_messages).to include("Telephone number is under 11 degit")
+      end
+    end
+  end
 end
