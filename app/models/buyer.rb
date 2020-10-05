a95a3b9fa1a3b961dea3b28860e37e95167f0691
ryo-suemura <ryosuemura@gmail.com>
2020-10-05 11:35:51 +0900

    Activehashの設定

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,3 +1,9 @@
 class Buyer < ApplicationRecord
   belongs_to :purchase
+  attr_accessor :token
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :place
+
+
 end
