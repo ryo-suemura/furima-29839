57a1e5b06120bfe0b247d5ddaa47a2eab7442195
ryo-suemura <ryosuemura@gmail.com>
2020-10-05 11:34:13 +0900

    コントローラーの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,16 @@
 class PurchasesController < ApplicationController
+  before_action :setting
+  before_action :move_to_index
 
   def index
-   @item = Item.find(params[:item_id])
+   @buyer_purchase = BuyerPurchase.new
   end
 
   def create
-    @purchase = Purchase.new(purchase_params)
-    if @purchase.valid?
-      @purchase.save
+    @buyer_purchase = BuyerPurchase.new(purchase_params)
+    if @buyer_purchase.valid?
+      pay_item
+      @buyer_purchase.save
       return redirect_to root_path
     else
       render 'index'
@@ -16,7 +19,28 @@ class PurchasesController < ApplicationController
 
   private
 
-  def purchase_params
-    params.require(:purchase).permit(:postal_code, :place_id, :city, :address, :building_name, :telephone_number)
+  def setting
+    @item = Item.find(params[:item_id])
   end
+
+  def purchase_params
+    params.require(:buyer_purchase).permit(:postal_code, :place_id, :city, :address, :building_name, :telephone_number, :purchase_id)
+    .merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy' 
+    )
+  end
+
+  def move_to_index
+    if  user_signed_in? != true || @item.user_id == current_user.id || @item.purchase.present?  
+      redirect_to root_path
+    end
+  end
+
 end
